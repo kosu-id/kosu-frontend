ce0ae5446f6644b14f896d68229633433613f79c
Rafly <muhr0417@gmail.com>
2025-08-05 13:25:49 +0700

    feat(landing): menambahkan fitur section

---
+++ app/(landing-page)/sections/FiturSection.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Judul from "../components/Judul";
+import CTASection from "./CTASection";
+import fiturKOSU from "../data/fitur";
+import FiturRow from "../components/FiturRow";
+
+export default function FiturSection() {
+  return (
+    <section className="my-5 md:my-20">
+      <Judul>Kenapa KOSU?</Judul>
+
+      <div className="border-2 p-6 md:p-10 rounded-xl">
+        <div className="flex justify-center md:justify-between md:mb-5">
+          <h2 className="text-xl md:text-3xl font-bold">Fitur</h2>
+          <div className="md:flex hidden gap-x-28 mr-7 text-xl font-bold">
+            <span className="bg-black text-white py-2 px-4 rounded-xl">
+              Tanpa KOSU
+            </span>
+            <span className="bg-black text-white py-2 px-4 rounded-xl">
+              Dengan KOSU
+            </span>
+          </div>
+        </div>
+
+        <hr className="border-gray-300" />
+
+        <div className="space-y-4">
+          {fiturKOSU.map((fitur, index) => (
+            <FiturRow
+              key={index}
+              index={index}
+              nama={fitur.nama}
+              tanpaKOSU={fitur.tanpaKOSU}
+              denganKOSU={fitur.denganKOSU}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-10">
+        <CTASection />
+      </div>
+    </section>
+  );
+}
